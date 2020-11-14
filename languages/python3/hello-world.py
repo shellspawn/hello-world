b64937349d102c021280bfc1efd0baf4f9809352
Shellspawn <57167466+shellspawn@users.noreply.github.com>
2020-11-14 03:41:38 +0000

    Create hello-world.py
    I know i could have done this in a simpler way but i figured i would take the opportunity to make this serve as a reminder on making a variable and function in python whilst i was at it.

---
+++ languages/python3/hello-world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Storing "Hello World!" in the variable "greeting"
+greeting = "Hello World!"
+
+# Defining the function that uses print() to print the variable greeting to the screen
+def greetfunc():
+    print(greeting)
+
+# Calling the function
+greetfunc():
